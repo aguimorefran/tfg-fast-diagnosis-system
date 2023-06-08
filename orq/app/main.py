0586b41908dc2017301fd5beccf021efa8d37273
Francisco Aguilera <aguimorefran@gmail.com>
2023-06-08 16:00:03 +0200

    + Ping

--- orq/app/main.py
+++ orq/app/main.py
@@ -1,41 +1,54 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-import requests
+from fastapi import FastAPI, HTTPException
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
+import requests
+import redis
 
 app = FastAPI()
 
+def check_redis(host='redis', port=6379):
+    try:
+        redis_client = redis.Redis(host=host, port=port)
+        if redis_client.ping():
+            return True
+        else:
+            return False
+    except redis.ConnectionError:
+        return False
+
 @app.get("/ping")
-def ping():
-    return JSONResponse(status_code=200, content={"message": "OK"})
+async def ping():
+    return {"ping": "pong"}
 
 @app.get("/pingall")
-def ping_all():
-    auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
-    cluster = Cluster(['cassandra'], port=9042, auth_provider=auth_provider)
-    session = cluster.connect()
-    
-    results = {}
-    
+async def ping_all():
+    redis_status = "up" if check_redis() else "down"
     try:
-        session.execute('SELECT * FROM system.local')
-        results['cassandra'] = "OK"
-    except Exception as e:
-        results['cassandra'] = f"Failed - {str(e)}"
-    
-    try:
-        r = requests.get('http://sasmock:8000/ping')
-        r.raise_for_status()
-        results['sasmock'] = "OK"
-    except Exception as e:
-        results['sasmock'] = f"Failed - {str(e)}"
+        cluster = Cluster(['cassandra'], port=9042, 
+                          auth_provider=PlainTextAuthProvider(username='cassandra', password='cassandra'))
+        session = cluster.connect()
+        session.execute('SELECT now() FROM system.local')
+        cassandra_status = "up"
+    except:
+        cassandra_status = "down"
         
     try:
-        r = requests.get('http://fca-engine:8005/ping')
-        r.raise_for_status()
-        results['fca_engine'] = "OK"
-    except Exception as e:
-        results['fca_engine'] = f"Failed - {str(e)}"
-    
-    return JSONResponse(status_code=200, content=results)
+        response_fds = requests.get("http://fca-engine:8005/ping")
+        response_fds.raise_for_status()
+        fds_status = "up"
+    except:
+        fds_status = "down"
+
+    try:
+        response_sas = requests.get("http://sasmock:8000/ping")
+        response_sas.raise_for_status()
+        sas_status = "up"
+    except:
+        sas_status = "down"
+
+    return {
+        "fca-engine": fds_status,
+        "cassandra": cassandra_status,
+        "sasmock": sas_status,
+        "redis": redis_status
+    }
